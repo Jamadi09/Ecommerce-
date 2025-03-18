0b2f6e8296b0a4c6aab8629282d5386705c350ed
Jamadi09 <121224656+Jamadi09@users.noreply.github.com>
2025-03-18 14:28:53 +0100

    Update ListProduct.jsx

--- Admin/src/Components/ListProduct/ListProduct.jsx
+++ Admin/src/Components/ListProduct/ListProduct.jsx
@@ -45,7 +45,7 @@ const ListProduct = () => {
         {allproducts.map((product, index)=>{
             return <>
             <div key={index} className="listproduct-format-main listproduct-format">
-               <img src=https://ecommerce-backend1-eaty.onrender.com/images/product.image alt="" className="listproduct-product-icon" />
+               <img src={product.image} alt="" className="listproduct-product-icon" />
                <p>{product.name}</p>
                <p>#{product.old_price}</p>
                <p>#{product.new_price}</p>
