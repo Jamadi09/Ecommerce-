12f3b3b535e321285bfd4cb50d2938ce8b0a6080
Jamadi09 <121224656+Jamadi09@users.noreply.github.com>
2025-03-20 07:35:53 +0100

    Update ListProduct.jsx

--- Admin/src/Components/ListProduct/ListProduct.jsx
+++ Admin/src/Components/ListProduct/ListProduct.jsx
@@ -7,7 +7,7 @@ const ListProduct = () => {
     const [allproducts, setAllProducts] = useState([]);
 
     const fetchInfo = async ()=>{
-        await fetch('https://ecommerce-backend1-eaty.onrender.com/allproducts')
+        await fetch('https://ecommerce-backend-ldbc.onrender.com/allproducts')
         .then((res)=>res.json())
         .then((data)=>{setAllProducts(data)});
 
@@ -18,7 +18,7 @@ const ListProduct = () => {
      },[])
 
      const remove_product = async (id)=>{
-        await fetch('https:https://ecommerce-backend1-eaty.onrender.com/removeproduct',{
+        await fetch('https://ecommerce-backend-ldbc.onrender.com/removeproduct',{
             method:'POST',
             headers:{
                 Accept: 'Application/json',
@@ -45,7 +45,7 @@ const ListProduct = () => {
         {allproducts.map((product, index)=>{
             return <>
             <div key={index} className="listproduct-format-main listproduct-format">
-               <img src={product.image.replace("https://ecommerce-backend1-eaty.onrender.com")} alt="" className="listproduct-product-icon" />
+               <img src={product.image.replace("https://ecommerce-backend-ldbc.onrender.com")} alt="" className="listproduct-product-icon" />
                <p>{product.name}</p>
                <p>#{product.old_price}</p>
                <p>#{product.new_price}</p>
