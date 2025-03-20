c9eb01a5876c1ba4c28c6ee8a9933de72000cd1e
Jamadi09 <121224656+Jamadi09@users.noreply.github.com>
2025-03-20 07:42:37 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || https://ecommerce-backend-ldbc.onrender.com;
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
